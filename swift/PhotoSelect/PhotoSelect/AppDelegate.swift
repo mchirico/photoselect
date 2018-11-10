db46988002c1b3d843f64dcb6c056f96132b6173
Mike Chirico (Chico) <mchirico@t.aipiggybot.io>
2018-11-10 18:05:34 -0500

    AirDrop

--- swift/PhotoSelect/PhotoSelect/AppDelegate.swift
+++ swift/PhotoSelect/PhotoSelect/AppDelegate.swift
@@ -10,12 +10,33 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+  
   var window: UIWindow?
-
+  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
     return true
   }
+  
+  // MARK: - Handle File Sharing
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    // We can do a lot more here...
+    print("URL: ")
+    
+    switch url.pathExtension {
+    case "sqlite":
+      print("sqlite")
+    case "Zsqlite":
+      print("Zsqlite")
+    case "photoSelect":
+      print("photoSelect")
+    default:
+      return false
+    }
 
+    ViewController.importData(from: url)
+    
+    return true
+  }
+  
 }
